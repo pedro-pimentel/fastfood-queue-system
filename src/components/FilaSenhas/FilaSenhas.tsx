8236e0a2b609c6a16bc8683280cc3c976b9e7eb2
Pedro Pimentel <pedroamazonet@gmail.com>
2024-07-13 11:47:03 -0300

    style(index.css): adição do flowbite como framework css

--- src/components/FilaSenhas/FilaSenhas.tsx
+++ src/components/FilaSenhas/FilaSenhas.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'flowbite';
 import './FilaSenhas.css';
 
 interface Senha {
@@ -19,19 +20,34 @@ const FilaSenhas: React.FC<FilaSenhasProps> = ({ titulo, senhas, ultimaSenhaCham
   const sortedSenhas = [...senhas].sort((a, b) => parseInt(b.id.split('-')[1]) - parseInt(a.id.split('-')[1]));
 
   return (
-    <div className="fila-container">
-      <h2>{titulo}</h2>
-      <ul>
-        {sortedSenhas.map((senha) => (
-          <li key={senha.id} className={`senha ${senha.status} ${senha.id === ultimaSenhaChamada ? 'destaque' : ''}`}>
-            <span>{senha.numero}</span>
-          </li>
-        ))}
-      </ul>
-      {ultimaSenhaChamada && onRemoverUltimaSenhaChamada && (
-        <button className="remove-button" onClick={onRemoverUltimaSenhaChamada}>Remover Última Senha Chamada</button>
-      )}
-    </div>
+    // <div className="fila-container">
+      <div className=" fila-container w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex items-center justify-between mb-4">
+          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">{titulo}</h5>
+        </div>
+        <div className="flow-root">
+          <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+            {sortedSenhas.map((senha) => (
+              <li key={senha.id} className={`py-3 sm:py-4 senha ${senha.status} ${senha.id === ultimaSenhaChamada ? 'destaque' : ''}`}>
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <img className="w-8 h-8 rounded-full" src={process.env.PUBLIC_URL + '/LOGO_X-BOM.png'} alt="Logo X-BOM"/>
+                  </div>
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      <span>{senha.numero}</span>
+                    </p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {ultimaSenhaChamada && onRemoverUltimaSenhaChamada && (
+            <button className="remove-button" onClick={onRemoverUltimaSenhaChamada}>Remover Última Senha Chamada</button>
+          )}
+        </div>
+      </div>
+    // </div>
   );
 };
 
--- src/components/FormularioSenha/FormularioSenha.tsx
+++ src/components/FormularioSenha/FormularioSenha.tsx
@@ -35,7 +35,9 @@ const FormularioSenha: React.FC<FormularioSenhaProps> = ({ onAddSenha }) => {
           placeholder="Digite a senha"
         />
       </label>
-      <button type="submit">Adicionar Senha</button>
+      {/* <button type="submit">Adicionar Senha</button> */}
+      <button type="submit" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Adicionar Senha</button>
+
     </form>
   );
 };
