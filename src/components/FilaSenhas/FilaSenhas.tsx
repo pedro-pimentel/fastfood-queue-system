8b779ca428a6de48a8a87f839bb8f8029894e97c
Pedro Pimentel <pedroamazonet@gmail.com>
2024-07-16 08:45:30 -0300

    style(filasenhas.tsx, filasenhas.css): mudança de tamanho da senha destaque

--- src/components/FilaSenhas/FilaSenhas.tsx
+++ src/components/FilaSenhas/FilaSenhas.tsx
@@ -20,38 +20,31 @@ const FilaSenhas: React.FC<FilaSenhasProps> = ({ titulo, senhas, ultimaSenhaCham
   const sortedSenhas = [...senhas].sort((a, b) => parseInt(b.id.split('-')[1]) - parseInt(a.id.split('-')[1]));
 
   return (
-      <div className=" fila-container w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex items-center justify-between mb-4">
-          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">{titulo}</h5>
-        </div>
-        <div className="flow-root">
-          <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-            {sortedSenhas.map((senha) => (
-              <li key={senha.id} className={`py-3 sm:py-4 senha ${senha.status} ${senha.id === ultimaSenhaChamada ? 'destaque' : ''}`}>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img className="w-8 h-8 rounded-full" src={process.env.PUBLIC_URL + '/LOGO_X-BOM.png'} alt="Logo X-BOM"/>
-                  </div>
-                  <div className="flex-1 min-w-0 ms-4">
-                    <p className="text-sm font-medium text-gray-900 dark:text-white">
-                      <span>{senha.numero}</span>
-                    </p>
-                  </div>
-                  <div className="flex-1 min-w-0 ms-4">
-                  {onRechamarSenha && (
-                    <button className="rechamar-button" onClick={() => onRechamarSenha(senha)}>Rechamar</button>
-                  )}
-                  {onRemoverSenha && (
-                    <button className="remove-button" onClick={() => onRemoverSenha(senha)}>Remover</button>
-                  )}
-                  </div>
-                  
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="fila-container w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">{titulo}</h5>
       </div>
+      <div className="flow-root">
+        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+          {sortedSenhas.map((senha) => (
+            <li key={senha.id} className={`py-3 sm:py-4 senha ${senha.status} ${senha.id === ultimaSenhaChamada ? 'destaque' : ''}`}>
+              <div className="flex items-center justify-center">
+                <img className="w-8 h-8 rounded-full" src={process.env.PUBLIC_URL + '/LOGO_X-BOM.png'} alt="Logo X-BOM"/>
+                <span className="ml-4 text-2xl font-bold">{senha.numero}</span>
+              </div>
+              <div className="flex justify-end mt-2 space-x-2">
+                {onRechamarSenha && (
+                  <button className="rechamar-button" onClick={() => onRechamarSenha(senha)}>Rechamar</button>
+                )}
+                {onRemoverSenha && (
+                  <button className="remove-button" onClick={() => onRemoverSenha(senha)}>Remover</button>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
